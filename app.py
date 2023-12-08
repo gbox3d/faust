0a08055c80507d92f536767f2aa3deb7a36e8c8c
gbox3d <gbox3d@gmail.com>
2023-12-08 19:22:21 +0900

    document

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #%%
+__version__ = (0,0,1)
 packetHeaderCheckCode = 20231208
+# 큐 생성
+request_queue = queue.Queue()
 
 model_name = os.getenv('MODEL_NAME')
 print(f'Start loading {model_name}')
@@ -30,21 +33,17 @@ hf_pipeline = pipeline(
     torch_dtype=torch.float16,
     # load_in_8bit=True,
     do_sample=False,
-    repeat_penalty=1.15,
+    #repeat_penalty=1.15,
     # device_map="auto" # GPU 상황에 맞게 자동으로 설정
     device_map="auto"  # GPU 0사용 설정)
 )
 
 print(f'Load time: {time.time() - start_tick}')
-
 model = hf_pipeline.model
-
-__version__ = (0,0,1)
 #%%
 def generate_text_interactively(prompt, max_length=128,conn=None,_checkCode=packetHeaderCheckCode):
     input_ids = tokenizer.encode(prompt, return_tensors="pt").to(model.device)
     generated = input_ids
-    # sentences = set()
     
     if conn != None:
         # conn.sendall("##START##".encode())
@@ -63,17 +62,7 @@ def generate_text_interactively(prompt, max_length=128,conn=None,_checkCode=pack
                 next_token_logits = outputs.logits[:, -1, :]
                 next_token = torch.argmax(next_token_logits, dim=-1, keepdim=True)
                 generated = torch.cat([generated, next_token], dim=-1)
-
-                # 현재까지 생성된 텍스트 확인
-                # generated_text = tokenizer.decode(generated[0], skip_special_tokens=True)
-
-                # 모든 문장 분리
-                # current_sentences = set(generated_text.split('.'))
-                # if sentences.intersection(current_sentences):
-                #     print('stop generating text : duplicated sentence : ',current_sentences)
-                #     break
-                # sentences.update(current_sentences)
-
+                
                 # 다음 토큰을 텍스트로 변환하여 출력
                 next_token_text = tokenizer.decode(next_token[0], skip_special_tokens=True)
                 print(next_token_text)
@@ -101,14 +90,6 @@ def generate_text_interactively(prompt, max_length=128,conn=None,_checkCode=pack
     except Exception as e:
         print(e)
         return None
-        
-    # OnDone(generated_text)
-# #%%
-# # 예시 사용
-# prompt = "지구의 위성은 무엇이 있을까요?"
-# generate_text_interactively(prompt, max_length=20)
-# 큐 생성
-request_queue = queue.Queue()
 
 #%%
 def onPacket(conn, data, rinfo):
@@ -117,7 +98,6 @@ def onPacket(conn, data, rinfo):
         data += conn.recv(8 - len(data))
     
     _checkCode,cmd = unpack("<LB",data[:5])
-    
     
     if _checkCode != packetHeaderCheckCode :
         print(f'checkcode error : {_checkCode}')
@@ -172,21 +152,11 @@ try :
             print(f'prompt : {req_data["prompt"]}')
             
             _gen_text = generate_text_interactively(
-                
-                req_data['prompt'],conn=req_data['conn']
+                prompt=req_data['prompt'],
+                conn=req_data['conn']
                 )
             
             print(f'generated_text : {_gen_text}')
-            
-            # req_data['conn'].sendall("##FIN##".encode())
-            # generated_text = generate_text_interactively(prompt['prompt'], max_length=1024)
-            # print(f'generated_text : {generated_text}')
-            # prompt['conn'].sendall(generated_text.encode())
-            
-        
-        #pipeline 처리 
-        pass
-            
 except KeyboardInterrupt:        
     print('KeyboardInterrupt')        
 except Exception as e:
--- readme.md
+++ readme.md
@@ -3,6 +3,75 @@
 Faust 는 LLM을 서비스 하기 위한 도구입니다.  
 huggingface의 transformers 라이브러리를 사용하여 모델을 로드하고, TCP 소켓을 통해 텍스트를 입력받아 모델을 통해 텍스트를 생성하고 다시 TCP 소켓을 통해 텍스트를 전송합니다.  
 
+## 사용법
+
+### vscode luanche.json 설정
+```json
+{
+    "name": "Faust LLMPlayer",
+    "type": "python",
+    "request": "launch",
+    "program": "${workspaceFolder}/faust/app.py",
+    "console": "integratedTerminal",
+    "cwd": "${workspaceFolder}/faust",
+    "justMyCode": false
+}
+```
+
+### 실행하기  
+
+.env 파일을 생성하고 다음과 같이 실행합니다.  
+
+
+```bash
+python app.py
+```
+
+## Protocol
+
+모든 패킷은 다음 구조를 따릅니다:<br>
+
+### 공통헤더
+
+헤더 체크 코드 (unsigned long, 0:4): 모든 패킷은 20231208로 시작합니다.  
+명령어 (unsigned char, 4:5): 패킷의 유형을 나타냅니다.  
+예약 (unsigned char, 5:8): 명령어에 따라 예약되어 있습니다.  
+
+
+### 클라이언트 요청
+
+**0x10 명령어**  
+텍스트 생성 요청. 데이터는 UTF-8로 인코딩된 텍스트입니다.  
+헤더 체크 코드 (unsigned long, 0:4): 20231208로 설정합니다.  
+명령어 (unsigned char, 4:5): 0x10으로 설정합니다.  
+예약 (unsigned char, 5:6): 0으로 설정합니다.  
+데이터 길이 (unsigned short, 6:8 바이트): 이어지는 데이터의 길이 (바이트 단위).<br>
+데이터 (bytes , 8: ): 실제 데이터.<br>
+
+**0x20 명령어**    
+요청 큐의 길이 요청. 서버는 요청 큐의 현재 길이를 응답합니다.    
+헤더 체크 코드 (unsigned long, 0:4): 20231208로 설정합니다.  
+명령어 (unsigned char, 4:5): 0x20으로 설정합니다.  
+예약 (unsigned char, 5:8): 0으로 설정합니다.  
+
+### 서버 응답
+**0x20 응답**  
+요청 대기큐 길이 길이 (unsigned short, 6:8 바이트): 이어지는 데이터의 길이 (바이트 단위).  
+
+**0x10 응답**  
+생성 시작 알림  
+(unsigned char, 5:8): version  
+
+**0x11 응답**  
+토큰생성  
+(unsigned short, 6:8): 토큰 길이  
+(bytes, 8: ): 토큰  
+
+
+**0x12 응답**  
+토콘 생성 완료  
+(unsigned char, 6:8): 생성된 전체 토큰 길이  
+
 
 ## 기타
 
